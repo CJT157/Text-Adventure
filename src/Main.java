d0e5b4c96967ab9e4128d200da5a8419a0dac267
CJT157 <cjaytruemner@gmail.com>
2020-04-14 12:36:37 -0400

    Added message print with specified items

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
+	public static Inventory inv = new Inventory();
+	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner sc = new Scanner(System.in);
-		Inventory inv = new Inventory();
 		inv.readItems();
-
-		System.out.println("Hello, there's a carrot, apple, and can on the ground");
 		
+		String[] items;
+
+		print("There is an apple, carrot, and can on the ground", "apple,carrot,can");
+		System.out.println(inv.toString());
+		print("test test", "");
 	}
 	
-	public static void read(String response) {
+	public static void read(String[] response, String[] itemList) {
+		boolean inList = false;
 		
+		for (String i : itemList) {
+			if (response[1].equals(i)) {
+				inList = true;
+				break;
+			}
+		}
+		
+		if (response[0].equals("take")) {
+			inv.addItem(response[1]);
+		}
+	}
+	
+	public static void print(String text, String items) {
+		String[] itemList = items.split(","); 
+		System.out.println(text);
+		read(sc.nextLine().split(" "), itemList);
 	}
 }
